af363deb994b8e72af19edd870dff2581b50bf52
newerstore <neweraimportss2@gmail.com>
2026-05-25 23:58:19 -0300

    Update calculate-shipping.js

--- api/calculate-shipping.js
+++ api/calculate-shipping.js
@@ -9,52 +9,33 @@ export default async function handler(req, res) {
 
   try {
 
-    /* GERA TOKEN */
-    const authResponse = await fetch('https://melhorenvio.com.br/oauth/token', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        grant_type: 'client_credentials',
-        client_id: process.env.MELHOR_ENVIO_CLIENT_ID,
-        client_secret: process.env.MELHOR_ENVIO_CLIENT_SECRET
-      })
-    });
-
-    const authData = await authResponse.json();
-
-    console.log('TOKEN:', authData);
-
-    const token = authData.access_token;
-
-    if (!token) {
-      return res.status(400).json({
-        success: false,
-        auth_error: authData
-      });
-    }
-
-    /* CEP */
     const body = req.body || {};
     const cep = body.cep?.replace(/\D/g, '');
 
-    /* CALCULA FRETE */
+    if (!cep) {
+      return res.status(400).json({
+        success: false,
+        message: 'CEP obrigatório'
+      });
+    }
+
     const response = await fetch('https://melhorenvio.com.br/api/v2/me/shipment/calculate', {
       method: 'POST',
       headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json',
-        'Authorization': `Bearer ${token}`,
+        'Authorization': `Bearer ${process.env.MELHOR_ENVIO_TOKEN}`,
         'User-Agent': 'NEWER STORE'
       },
       body: JSON.stringify({
         from: {
           postal_code: process.env.STORE_ORIGIN_CEP
         },
+
         to: {
           postal_code: cep
         },
+
         products: [
           {
             id: '1',
@@ -70,8 +51,6 @@ export default async function handler(req, res) {
     });
 
     const data = await response.json();
-
-    console.log('FRETE:', data);
 
     return res.status(200).json({
       success: true,
